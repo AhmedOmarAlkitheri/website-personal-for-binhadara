c32773018daaaf0b02642da7c85ff70f16381145
AhmedOmarAlkitheri <137421786+AhmedOmarAlkitheri@users.noreply.github.com>
2025-04-26 16:07:34 +0300

    change

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -9,6 +9,7 @@ interface HeroSectionProps {
 
 const HeroSection: React.FC<HeroSectionProps> = ({ scrollToSection }) => {
   return (
+    
     <section className="min-h-screen flex items-center relative bg-gradient-to-br from-white to-portfolio-light px-6 md:px-10 py-20">
       <div className="container mx-auto grid md:grid-cols-2 gap-10 items-center">
         <div className="order-2 md:order-1 animate-fade-in" style={{ animationDelay: "0.2s" }}>
@@ -40,7 +41,7 @@ const HeroSection: React.FC<HeroSectionProps> = ({ scrollToSection }) => {
         <div className="order-1 md:order-2 flex justify-center animate-fade-in" style={{ animationDelay: "0.4s" }}>
           <div className="relative w-64 h-64 md:w-80 md:h-80 rounded-full overflow-hidden border-4 border-white shadow-lg">
             <img 
-            
+
               src="images/personImage.png" 
               alt="Eng.BinHadara" 
               className="w-full h-full object-cover"
