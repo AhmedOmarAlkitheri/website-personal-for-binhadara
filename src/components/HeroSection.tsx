7027fbdb5235a759dcfaea6348d6d02c7fe8333f
AhmedOmarAlkitheri <137421786+AhmedOmarAlkitheri@users.noreply.github.com>
2025-04-30 16:15:47 +0300

    v

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -44,7 +44,7 @@ const HeroSection: React.FC<HeroSectionProps> = ({ scrollToSection }) => {
           <div className="relative w-64 h-64 md:w-80 md:h-80 rounded-full overflow-hidden border-4 border-white shadow-lg">
             <img 
            //   src="/images/personImage.png" 
-              src={window.location.origin + "/public/images/personImage.png"} 
+              src={window.location.origin + "/images/personImage.png"} 
               alt="Eng.BinHadara" 
               className="w-full h-full object-cover"
             /> 
